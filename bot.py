99b7a8634da39e1b886db4e277727da7246147ca
dhanush00005 <122075054+dhanush00005@users.noreply.github.com>
2024-09-08 11:39:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,6 +99,9 @@ async def approve_all(_, message):
     async def run_approve():
         try:
             await user.approve_all_chat_join_requests(message.chat.id)
+            img = random.choice(gif)
+            await app.send_video(m.from_user.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @VJ_Botz __**".format(m.from_user.mention, m.chat.title))
+            add_user(m.from_user.id)
         except FloodWait as t:
             await asyncio.sleep(t.value)
             await run_approve()
